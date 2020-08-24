8f9f519f676e48b42f3ea430249d061b21cd5c2b
Pankaj Kumar <pankajkumarmath1@gmail.com>
2020-08-24 08:02:10 +0530

    Add files via upload

---
+++ codechef/practice/CENS20A.cpp
@@ -0,0 +1,130 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll ;
+typedef set<pair<int,int>> spi;
+typedef set<pair<ll,ll>> spl;
+typedef vector<pair<int,int>> vpi;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<pair<ll,ll>> vpl;
+typedef vector<string> vs;
+typedef map<int,int>mi;
+typedef map<ll,ll> ml;
+typedef set<string> ss;
+typedef set<char>sc;
+typedef set<int> si;
+typedef set<ll> sl;
+#define pan cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);
+// define values.
+#define mod 1e9+7LL
+#define phi 1.618
+/* Bit-Stuff */
+#define get_set_bits(a) (__builtin_popcount(a))
+#define get_set_bitsll(a) ( __builtin_popcountll(a))
+#define get_trail_zero(a) (__builtin_ctz(a))
+#define get_lead_zero(a) (__builtin_clz(a))
+#define get_parity(a) (__builtin_parity(a))
+/*  Abbrevations  */
+#define ff first
+#define ss second
+#define mp make_pair
+#define line cout<<endl;
+#define pb push_back
+#define Endl "\n"
+// loops
+#define loop(i,start,end) for(ll i=ll(start);i<ll(end);i++)
+#define loop0(num) for(ll i=0;i<ll(num);i++)
+#define forin(arr,n) for(ll i=0;i<n;i++) cin>>arr[i];
+// Some print
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+#define cc ll test;cin>>test;while(test--)
+// sort
+#define all(V) (V).begin(),(V).end()
+#define srt(V) sort(all(V))
+#define srtGreat(V) sort(all(V),greater<ll>())
+// function
+
+ll power(ll x,   ll y, ll p)  
+{  
+    ll res = 1;      
+    x = x % p;             
+    while (y > 0)  
+    {    
+        if (y & 1)  
+            res = (res*x) % p;   
+        y = y>>1; 
+        x = (x*x) % p;  
+    }  
+    return res;  
+} 
+/* ascii value 
+A=65,Z=90,a=97,z=122 1=49
+*/
+/*  -----------------------------------------------------------------------------------*/
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<string> s(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s[i];
+    }
+    vector<vector<int>> v(1005, vector<int>(1005));
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int x1, x2, y1, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        v[x1][y1]++;
+        v[x1][y2 + 1]--;
+        v[x2 + 1][y1]++;
+        v[x2 + 1][y2 + 1]--;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            v[i][j] += v[i][j - 1];
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            v[i][j] += v[i - 1][j];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (v[i + 1][j + 1] % 2 == 0)
+            {
+                cout << s[i][j];
+            }
+            else
+            {
+                cout << ((s[i][j] - '0') ^ 1);
+            }
+        }
+        cout << '\n';
+    }
+}
+
+int main()
+{
+    pan
+    solve();
+}
