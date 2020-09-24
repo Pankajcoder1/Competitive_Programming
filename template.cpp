249dd1c96c64dfd64337227c81608c28550c5263
Pankajcoder1 <pankajkumarmath1@gmail.com>
2020-09-24 19:12:15 +0530

    changes

--- template.cpp
+++ template.cpp
@@ -4,25 +4,18 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
+// policy based ds
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 typedef long long ll ;
-typedef set<pair<int,int>> spi;
-typedef set<pair<ll,ll>> spl;
-typedef vector<pair<int,int>> vpi;
-typedef vector<int> vi;
 typedef vector<ll> vl;
 typedef vector<bool> vb;
 typedef vector<char> vc;
-typedef vector<pair<ll,ll>> vpl;
-typedef vector<string> vs;
-typedef map<int,int>mi;
 typedef map<ll,ll> ml;
-typedef set<string> ss;
-typedef set<char>sc;
-typedef set<int> si;
-typedef set<ll> sl;
 #define pan cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);
 // define values.
-#define mod 1000000007
+#define mod 10000009
 #define phi 1.618
 /* Bit-Stuff */
 #define get_set_bits(a) (__builtin_popcount(a))
@@ -38,8 +31,6 @@ typedef set<ll> sl;
 #define pb push_back
 #define Endl "\n"
 // loops
-#define loop(i,start,end) for(ll i=ll(start);i<ll(end);i++)
-#define loop0(num) for(ll i=0;i<ll(num);i++)
 #define forin(arr,n) for(ll i=0;i<n;i++) cin>>arr[i];
 // Some print
 #define no cout<<"NO"<<endl;
@@ -54,21 +45,38 @@ typedef set<ll> sl;
 ll power(ll x,ll y)
 {
 	ll res=1;
+	// x=x%mod;
 	while(y>0)
 	{
-		if(y%2==1)res*=x;
-		y/=2; x*=x;
+		if(y%2==1)
+		{
+			res*=x;
+			// res=res%mod;
+		}
+		y/=2; x*=x; // x=x%mod;
 	}
 	return res;
 }
+// datatype definination
+#define ordered_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+
 /* ascii value 
 A=65,Z=90,a=97,z=122
 */
 /*  -----------------------------------------------------------------------------------*/
 
+ll solve()
+{
+
+	return 0;
+}
+
 int main()
 {
 	//freopen("input.txt"a, "r", stdin);
 	pan;
-	
+	cc
+	{
+		solve();
+	}
 }
