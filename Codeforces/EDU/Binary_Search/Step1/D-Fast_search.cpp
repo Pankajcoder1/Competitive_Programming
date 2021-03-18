71f247284882bb050a7358b5ef57541a7787628a
Pankajcoder1 <pankajkumarmath1@gmail.com>
2021-03-18 11:22:30 +0530

    binary search edu sectioin codeforces step 1 complete

---
+++ Codeforces/EDU/Binary_Search/Step1/D-Fast_search.cpp
@@ -0,0 +1,149 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+    Institute: National Institute of Technology, Uttarakhand
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll ;
+typedef vector<ll> vl;
+#define speed cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);
+/*  Abbrevations  */
+#define ff first
+#define ss second
+#define mp make_pair
+#define line cout<<endl;
+#define pb push_back
+#define Endl "\n"
+// loops
+#define forin(arr,n) for(ll i=0;i<n;i++) cin>>arr[i];
+// Some print
+#define no cout<<"No"<<endl;
+#define yes cout<<"Yes"<<endl;
+// sort
+#define all(V) (V).begin(),(V).end()
+#define srt(V) sort(all(V))
+#define srtGreat(V) sort(all(V),greater<ll>())
+// some extra
+#define printv(v) for(ll i=0;i<ll(v.size());i++){cout<<v[i]<<" ";} line;
+#define precision(x) cout<<fixed<<setprecision(x);
+#define sz(V) ll(V.size())
+// template
+template <typename T>
+T mymax(T x,T y)
+{
+    return (x>y)?x:y;
+}
+// function
+ll power(ll x,ll y,ll mod)
+{
+    ll res=1;
+    // x=x%mod;
+    while(y>0)
+    {
+        if(y%2==1)
+        {
+            res*=x;
+            // res=res%mod;
+        }
+        y/=2; x*=x; // x=x%mod;
+    }
+    return res;
+}
+ll str_to_num(string s)
+{
+    return stoi(s);
+}
+
+string num_to_str(ll num)
+{
+    return to_string(num);
+}
+// datatype definination
+#define ordered_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+class Point
+{
+    public:
+        ll x;
+        ll y;
+        ll z;
+        ll getsum()
+        {
+            return x+y+z;
+        }
+};
+/* ascii value 
+A=65,Z=90,a=97,z=122
+*/
+/*  --------------------MAIN PROGRAM----------------------------*/
+// to run ctrl+b
+ll searchl(ll num,vector<ll>& v,ll start,ll end){
+    while(end>start+1){
+        ll mid=(start+end)/2;
+        if(v[mid]>=num)
+            end=mid;
+        else
+            start=mid;
+    }
+    return end+1;
+}
+
+ll searchr(ll num,vector<ll>& v,ll start,ll end){
+    while(end>start+1){
+        ll mid=(start+end)/2;
+        if(v[mid]<=num)
+            start=mid;
+        else
+            end=mid;
+    }
+    return start+1;
+}
+
+ll solve()
+{
+    ll n;
+    cin>>n;
+    vl v(n);
+    forin(v,n);
+    ll k;
+    cin>>k;
+    srt(v);
+    while(k--){
+        ll l,r;
+        cin>>l>>r;
+        ll ans1=searchl(l,v,-1,n);
+        ll ans2=searchr(r,v,-1,n);
+        ll ans=abs(ans1-ans2)+1;
+        if(ans1>ans2)
+            ans=0;
+        // cout<<"ans 1 and 2 is "<<ans1<<" "<<ans2<<endl;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+
+int main()
+{
+    speed;
+    /* #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif */
+    ll TestCase=1;
+    // cin>>TestCase;
+    while(TestCase--)
+    {
+        solve();
+    }
+}
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission 
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
