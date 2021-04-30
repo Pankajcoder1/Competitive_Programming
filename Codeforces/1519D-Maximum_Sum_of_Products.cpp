f07edeb2094cb0593e8c152b86772858ec8d7a57
Pankajcoder1 <pankajkumarmath1@gmail.com>
2021-04-30 22:29:42 +0530

    april lunchtime codechef

---
+++ Codeforces/1519D-Maximum_Sum_of_Products.cpp
@@ -0,0 +1,168 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+    Institute: National Institute of Technology, Uttarakhand
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll ;
+typedef unsigned long long ull;
+typedef vector<ll> vl;
+#define speed cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);
+/*  Abbrevations  */
+#define ff first
+#define ss second
+#define mp make_pair
+#define line cout<<endl;
+#define pb push_back
+#define Endl "\n"
+// loops
+#define forin(arr,n) for(ll i=0;i<n;i++) cin>>arr[i];
+// Some print
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+// sort
+#define all(V) (V).begin(),(V).end()
+#define srt(V) sort(all(V))
+#define srtGreat(V) sort(all(V),greater<ll>())
+// some extra
+#define printv(v) for(ll i=0;i<ll(v.size());i++){cout<<v[i]<<" ";} line;
+#define precision(x) cout<<fixed<<setprecision(x);
+#define sz(V) ll(V.size())
+// template
+template <typename T>
+T mymax(T x,T y)
+{
+    return (x>y)?x:y;
+}
+// function
+ll power(ll x,ll y,ll mod)
+{
+    ll res=1;
+    // x=x%mod;
+    while(y>0)
+    {
+        if(y%2==1)
+        {
+            res*=x;
+            // res=res%mod;
+        }
+        y/=2; x*=x; // x=x%mod;
+    }
+    return res;
+}
+ll str_to_num(string s)
+{
+    stringstream pk(s);
+    ll num;
+    pk>>num;
+    return num;
+}
+
+string num_to_str(ll num)
+{
+    return to_string(num);
+}
+// datatype definination
+#define ordered_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+class Point
+{
+    public:
+        ll x;
+        ll y;
+        ll z;
+        ll getsum()
+        {
+            return x+y+z;
+        }
+};
+/* ascii value 
+A=65,Z=90,a=97,z=122
+*/
+/*  --------------------MAIN PROGRAM----------------------------*/
+// to run ctrl+b
+const ll INF=LONG_MAX;
+const ll mod1=1e9+7;
+const ll mod2=998244353;
+
+
+// Techniques
+// divide into cases, brute force, pattern finding
+// sort, greedy, binary search, two pointer
+// transform into graph
+
+ll solve()
+{
+    ll n;
+    cin>>n;
+    vl a(n),b(n);
+    forin(a,n);
+    forin(b,n);
+    vl from_begin(n),from_end(n);
+    vector<vl> dp(5001,vl(5001,0));
+    ll maxo=0;
+    ll pro=0;
+    for(ll i=0;i<n;i++){
+        ll temp=a[i]*b[i];
+        pro+=temp;
+        from_begin[i]=pro;
+    }
+    pro=0;
+    for(ll i=n-1;i>=0;i--){
+        ll temp=a[i]*b[i];
+        pro+=temp;
+        from_end[i]=pro;
+    }
+    for(ll length=1;length<=n;length++){
+        for(ll i=0;i<n;i++){
+            ll j=i+length;
+            if(j>=n)
+                break;
+            if(length==1)
+                dp[i][j]=a[i]*b[j]+a[j]*b[i];
+            else if(length==2)
+                dp[i][j]=a[i]*b[i+2]+a[i+1]*b[i+1]+a[i+2]*b[i];
+            else
+                dp[i][j]=dp[i+1][j-1]+a[i]*b[j]+a[j]*b[i];
+        }
+    }
+    maxo=from_begin[n-1];
+    for(ll i=0;i<n;i++){
+        for(ll j=i+1;j<n;j++){
+            ll subarray_sum=dp[i][j];
+            if(i>0)
+                subarray_sum+=from_begin[i-1];
+            if(j<(n-1))
+                subarray_sum+=from_end[j+1];
+            maxo=max(maxo,subarray_sum);
+        }
+    }
+    cout<<maxo<<endl;
+    return 0;
+}
+
+int main()
+{
+    speed;
+    /* #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif */
+    ll TestCase=1;
+    // cin>>TestCase;
+    while(TestCase--)
+    {
+        solve();
+    }
+}
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission 
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
