f60d36812c5a974ea4bc8b3394bf91d665dce691
Pankajcoder1 <pankajkumarmath1@gmail.com>
2021-05-01 22:25:07 +0530

    leetcode biweekly 51

---
+++ LeetCode/Biweekly/51/5730.Replace_All_Digits_with_Characters.cpp
@@ -0,0 +1,57 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+    Institute: National Institute of Technology, Uttarakhand
+*/
+typedef long long ll ;
+typedef unsigned long long ull;
+typedef vector<int> vl;
+/*  Abbrevations  */
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+#define Endl "\n"
+#define all(V) (V).begin(),(V).end()
+#define srt(V) sort(all(V))
+#define srtGreat(V) sort(all(V),greater<ll>())
+// some extra
+#define printv(v) for(ll i=0;i<ll(v.size());i++){cout<<v[i]<<" ";} cout<<endl;
+#define precision(x) cout<<fixed<<setprecision(x);
+#define sz(V) ll(V.size())
+
+/* ascii value 
+A=65,Z=90,a=97,z=122
+*/
+/*  --------------------MAIN PROGRAM----------------------------*/
+// to run ctrl+b
+const ll INF=LONG_MAX;
+const ll mod1=1e9+7;
+const ll mod2=998244353;
+
+// Techniques
+// divide into cases, brute force, pattern finding
+// sort, greedy, binary search, two pointer
+// transform into graph
+
+// add main code here
+class Solution {
+public:
+    string replaceDigits(string s) {
+        int n=sz(s);
+        for(ll i=1;i<n;i+=2){
+            s[i]=char(s[i-1]+ll(s[i]-'0'));
+        }
+        return s;
+    }
+};
+
+
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission 
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
---
+++ LeetCode/Biweekly/51/5732.Maximum_Element_After_Decreasing_and_Rearranging.cpp
@@ -0,0 +1,67 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+    Institute: National Institute of Technology, Uttarakhand
+*/
+typedef long long ll ;
+typedef unsigned long long ull;
+typedef vector<int> vl;
+/*  Abbrevations  */
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+#define Endl "\n"
+#define all(V) (V).begin(),(V).end()
+#define srt(V) sort(all(V))
+#define srtGreat(V) sort(all(V),greater<ll>())
+// some extra
+#define printv(v) for(ll i=0;i<ll(v.size());i++){cout<<v[i]<<" ";} cout<<endl;
+#define precision(x) cout<<fixed<<setprecision(x);
+#define sz(V) ll(V.size())
+
+/* ascii value 
+A=65,Z=90,a=97,z=122
+*/
+/*  --------------------MAIN PROGRAM----------------------------*/
+// to run ctrl+b
+const ll INF=LONG_MAX;
+const ll mod1=1e9+7;
+const ll mod2=998244353;
+
+// Techniques
+// divide into cases, brute force, pattern finding
+// sort, greedy, binary search, two pointer
+// transform into graph
+
+// add main code here
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        srt(arr);
+        int n=sz(arr);
+        arr[0]=1;
+        for(ll i=1;i<n;i++){
+            if(arr[i]>(arr[i-1]+1)){
+                arr[i]=arr[i-1]+1;
+            }
+        }
+        return arr[sz(arr)-1];
+    }
+};
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
+
+
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission 
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
