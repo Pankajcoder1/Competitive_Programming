5142494a19dd0989b895f771fcacf929e900c948
Pankajcoder1 <pankajkumarmath1@gmail.com>
2021-06-14 08:19:42 +0530

    cf latoken round

--- 450_DSA/1.cpp
+++
@@ -1,9 +0,0 @@
-string reverseWord(string str){
-    
-  //Your code here
-  string ans="";
-  for(int i=str.size()-1;i>=0;i--){
-      ans+=str[i];
-  }
-  return ans;
-}
---
+++ LeetCode/Biweekly/54/1893.Check_if_All_the_Integers_in_a_Range_Are_Covered.cpp
@@ -0,0 +1,72 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+    Institute: National Institute of Technology, Uttarakhand
+*/
+typedef long long ll ;
+typedef unsigned long long ull;
+typedef vector<int> vl;
+typedef vector<vector<ll>> vvl;
+/*  Abbrevations  */
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+#define Endl "\n"
+#define all(V) (V).begin(),(V).end()
+#define srt(V) sort(all(V))
+#define srtGreat(V) sort(all(V),greater<ll>())
+// some extra
+#define printv(v) for(ll i=0;i<ll(v.size());i++){cout<<v[i]<<" ";} cout<<endl;
+#define precision(x) cout<<fixed<<setprecision(x);
+#define sz(V) ll(V.size())
+
+/* ascii value 
+A=65,Z=90,a=97,z=122
+*/
+/* Some syntax 
+//Syntax to create a min heap for priority queue
+//priority_queue <int, vector<int>, greater<int>>pq;
+*/
+
+
+/*  --------------------MAIN PROGRAM----------------------------*/
+// to run ctrl+b
+const ll INF=1e18;
+const ll mod1=1e9+7;
+const ll mod2=998244353;
+
+// Techniques :
+// divide into cases, brute force, pattern finding
+// sort, greedy, binary search, two pointer
+// transform into graph
+
+// Experience :
+// Cp is nothing but only observation and mathematics.
+
+// add main code here
+class Solution {
+public:
+    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+        vector<bool>check(right+1,false);
+        for(int i=0;i<ranges.size();i++){
+            for(int j=ranges[i][0];j<=ranges[i][1];j++)
+                check[j]=true;
+        }
+        for(int i=left;i<=right;i++){
+            if(check[i]==false)
+                return false;
+        }
+        return true;
+    }
+};
+
+
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission 
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
