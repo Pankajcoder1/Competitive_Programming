aba36f22eae15d863a6d22b5e26507b9aba3ca22
Pankajcoder1 <pankajkumarmath1@gmail.com>
2021-06-24 23:55:47 +0530

    daily practice

---
+++ Codeforces/1266C-Diverse_Matrix.cpp
@@ -0,0 +1,180 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+    Institute: National Institute of Technology, Uttarakhand
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll ;
+typedef unsigned long long ull;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+#define speed cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);
+/*  Abbrevations  */
+#define ff first
+#define ss second
+#define mp make_pair
+#define line cout<<endl;
+#define pb push_back
+#define Endl "\n"
+// loops
+#define forin(arr,n) for(ll i=0;i<n;i++) cin>>arr[i];
+// Some print
+#define no cout<<"No"<<endl;
+#define yes cout<<"Yes"<<endl;
+// sort
+#define all(V) (V).begin(),(V).end()
+#define srt(V) sort(all(V))
+#define srtGreat(V) sort(all(V),greater<ll>())
+// some extra
+#define printv(v) for(ll i=0;i<ll(v.size());i++){cout<<v[i]<<" ";} line;
+#define precision(x) cout<<fixed<<setprecision(x);
+#define sz(V) ll(V.size())
+// template
+template <typename T>
+T mymax(T x,T y)
+{
+    return (x>y)?x:y;
+}
+// function
+ll power(ll x,ll y,ll mod)
+{
+    ll res=1;
+    // x=x%mod;
+    while(y>0)
+    {
+        if(y%2==1)
+        {
+            res*=x;
+            // res=res%mod;
+        }
+        y/=2; x*=x; // x=x%mod;
+    }
+    return res;
+}
+ll str_to_num(string s)
+{
+    return stoi(s);
+}
+
+string num_to_str(ll num)
+{
+    return to_string(num);
+}
+// datatype definination
+#define ordered_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+class Point
+{
+    public:
+        ll x;
+        ll y;
+        ll z;
+        ll getsum()
+        {
+            return x+y+z;
+        }
+};
+/* ascii value 
+A=65,Z=90,a=97,z=122
+*/
+/* Some syntax 
+//Syntax to create a min heap for priority queue
+//priority_queue <int, vector<int>, greater<int>>pq;
+*/
+
+
+/*  --------------------MAIN PROGRAM----------------------------*/
+// to run ctrl+b
+const ll INF=1e18;
+const ll mod1=1e9+7;
+const ll mod2=998244353;
+
+
+// Techniques :
+// divide into cases, brute force, pattern finding
+// sort, greedy, binary search, two pointer
+// transform into graph
+
+// Experience :
+// Cp is nothing but only observation and mathematics.
+
+ll solve()
+{
+    ll r,c;
+    cin>>r>>c;
+    vvl ans(r,vl(c,1));
+    if(r==1||c==1){
+        if(r==1&&c==1){
+            cout<<0<<endl;
+            return 0;
+        }
+        else if(r==1){
+            ll temp=2;
+            for(ll j=0;j<c;j++){
+                ans[0][j]=temp;
+                temp++;
+            }
+        }
+        else if(c==1){
+            ll temp=2;
+            for(ll i=0;i<r;i++){
+                ans[i][0]=temp++;
+            }
+        }
+        for(ll i=0;i<r;i++){
+            for(ll j=0;j<c;j++){
+                cout<<ans[i][j]<<" ";
+            }
+            line;
+        }
+    }
+    else{
+        
+        ll temp=1;
+        for(ll i=0;i<r;i++){
+            for(ll j=0;j<c;j++){
+                ans[i][j]*=temp;
+            }
+            temp++;
+        }
+        for(ll i=0;i<c;i++){
+            for(ll j=0;j<r;j++){
+                ans[j][i]*=temp;
+            }
+            temp++;
+        }
+        for(ll i=0;i<r;i++){
+            for(ll j=0;j<c;j++){
+                cout<<ans[i][j]<<" ";
+            }
+            line;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    speed;
+    /* #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif */
+    ll TestCase=1;
+    // cin>>TestCase;
+    while(TestCase--)
+    {
+        solve();
+    }
+}
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission 
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
---
+++ Codeforces/638B-Making_Genome_in_Berland.cpp
@@ -0,0 +1,159 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+    Institute: National Institute of Technology, Uttarakhand
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll ;
+typedef unsigned long long ull;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+#define speed cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);
+/*  Abbrevations  */
+#define ff first
+#define ss second
+#define mp make_pair
+#define line cout<<endl;
+#define pb push_back
+#define Endl "\n"
+// loops
+#define forin(arr,n) for(ll i=0;i<n;i++) cin>>arr[i];
+// Some print
+#define no cout<<"No"<<endl;
+#define yes cout<<"Yes"<<endl;
+// sort
+#define all(V) (V).begin(),(V).end()
+#define srt(V) sort(all(V))
+#define srtGreat(V) sort(all(V),greater<ll>())
+// some extra
+#define printv(v) for(ll i=0;i<ll(v.size());i++){cout<<v[i]<<" ";} line;
+#define precision(x) cout<<fixed<<setprecision(x);
+#define sz(V) ll(V.size())
+// template
+template <typename T>
+T mymax(T x,T y)
+{
+    return (x>y)?x:y;
+}
+// function
+ll power(ll x,ll y,ll mod)
+{
+    ll res=1;
+    // x=x%mod;
+    while(y>0)
+    {
+        if(y%2==1)
+        {
+            res*=x;
+            // res=res%mod;
+        }
+        y/=2; x*=x; // x=x%mod;
+    }
+    return res;
+}
+ll str_to_num(string s)
+{
+    return stoi(s);
+}
+
+string num_to_str(ll num)
+{
+    return to_string(num);
+}
+// datatype definination
+#define ordered_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+class Point
+{
+    public:
+        ll x;
+        ll y;
+        ll z;
+        ll getsum()
+        {
+            return x+y+z;
+        }
+};
+/* ascii value 
+A=65,Z=90,a=97,z=122
+*/
+/* Some syntax 
+//Syntax to create a min heap for priority queue
+//priority_queue <int, vector<int>, greater<int>>pq;
+*/
+
+
+/*  --------------------MAIN PROGRAM----------------------------*/
+// to run ctrl+b
+const ll INF=1e18;
+const ll mod1=1e9+7;
+const ll mod2=998244353;
+
+
+// Techniques :
+// divide into cases, brute force, pattern finding
+// sort, greedy, binary search, two pointer
+// transform into graph
+
+// Experience :
+// Cp is nothing but only observation and mathematics.
+
+string ans="";
+char graph[150];
+ll check[150];
+
+void dfs(char ch){
+    check[ch]=3;
+    if(check[graph[ch]]!=3&&graph[ch])
+        dfs(graph[ch]);
+    ans+=ch;
+}
+
+ll solve()
+{
+    ll n;
+    cin>>n;
+    for(ll i=0;i<n;i++){
+        string s;
+        cin>>s;
+        for(ll i=1;i<sz(s);i++){
+            graph[s[i-1]]=s[i];
+            check[s[i]]=2;
+        }
+        if(check[s[0]]!=2)
+            check[s[0]]=1;
+    }
+    for(char i='a';i<='z';i++){
+        if(check[i]==1)
+            dfs(i);
+    }
+    reverse(all(ans));
+    cout<<ans<<endl;
+    return 0;
+}
+
+int main()
+{
+    speed;
+    /* #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif */
+    ll TestCase=1;
+    // cin>>TestCase;
+    while(TestCase--)
+    {
+        solve();
+    }
+}
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission 
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
