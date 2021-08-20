84098a1eab83a41361cd203776d77ec33e01bed3
Pankajcoder1 <pankajkumarmath1@gmail.com>
2021-08-20 14:42:42 +0530

    Cf div 3

---
+++ LeetCode/Practice/1339.Maximum_Product_of_Splitted_Binary_Tree.cpp
@@ -0,0 +1,93 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+    Institute: National Institute of Technology, Uttarakhand
+*/
+typedef long long ll ;
+typedef unsigned long long ull;
+typedef vector<int> vl;
+typedef vector<vector<int>> vvl;
+#define speed cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);
+/*  Abbrevations  */
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+// loops
+#define forin(arr,n) for(ll i=0;i<n;i++) cin>>arr[i];
+// Some print
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+// sort
+#define all(V) (V).begin(),(V).end()
+#define srt(V) sort(all(V))
+#define srtGreat(V) sort(all(V),greater<ll>())
+// some extra
+#define printv(v) for(ll i=0;i<ll(v.size());i++){cout<<v[i]<<" ";} cout<<endl;
+#define precision(x) cout<<fixed<<setprecision(x);
+#define sz(V) ll(V.size())
+
+
+/* ascii value
+A=65,Z=90,a=97,z=122
+*/
+
+/* Some syntax 
+//Syntax to create a min heap for priority queue
+//priority_queue <int, vector<int>, greater<int>>pq;
+*/
+/*  --------------------MAIN PROGRAM----------------------------*/
+// to run ctrl+b
+const ll INF=1e18;
+const ll mod1=1e9+7;
+const ll mod2=998244353;
+// Techniques :
+// divide into cases, brute force, pattern finding
+// sort, greedy, binary search, two pointer
+// transform into graph
+
+// Experience :
+// Cp is nothing but only observation and mathematics.
+
+
+//Add main code here
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    long long total_sum=0,ans=0;
+    int dfs(TreeNode* root){
+        if(root==nullptr){
+            return 0;
+        }
+        int current_sum=dfs(root->left)+dfs(root->right)+root->val;
+        ans=max(ans,(total_sum-current_sum)*current_sum);
+        return current_sum;
+    }
+    int maxProduct(TreeNode* root) {
+        total_sum=dfs(root);
+        int dummy=dfs(root);
+        return ans%mod1;
+    }
+};
+
+
+
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
