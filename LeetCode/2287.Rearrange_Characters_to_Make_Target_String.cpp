ab016e35abf02dd70d5372186abaaa713893f954
Pankajcoder1 <pankajkumarmath1@gmail.com>
2022-06-05 09:49:55 +0530

    leetcode contest

---
+++ LeetCode/2287.Rearrange_Characters_to_Make_Target_String.cpp
@@ -0,0 +1,80 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+*/
+typedef long long ll ;
+typedef vector<int> vl;
+typedef vector<vector<int>> vvl;
+#define speed cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);
+/*  Abbrevations  */
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+// Some print
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+// sort
+#define all(V) (V).begin(),(V).end()
+#define srt(V) sort(all(V))
+#define srtGreat(V) sort(all(V),greater<ll>())
+// some extra
+#define printv(v) for(ll i=0;i<ll(v.size());i++){cout<<v[i]<<" ";} cout<<endl;
+#define precision(x) cout<<fixed<<setprecision(x);
+#define sz(V) ll(V.size())
+
+
+/* ascii value
+A=65,Z=90,a=97,z=122
+*/
+
+/* Some syntax 
+//Syntax to create a min heap for priority queue
+//priority_queue <int, vector<int>, greater<int>>pq;
+*/
+/*  --------------------MAIN PROGRAM----------------------------*/
+// to run ctrl+b
+const ll INF=1e18;
+const ll mod1=1e9+7;
+const ll mod2=998244353;
+// Techniques :
+// divide into cases, brute force, pattern finding
+// sort, greedy, binary search, two pointer
+// transform into graph
+
+// Experience :
+// Cp is nothing but only observation and mathematics.
+
+
+//Add main code here
+
+class Solution {
+public:
+    int rearrangeCharacters(string s, string target) {
+        map<char,int> mp1,mp2;
+        int ans=INT_MAX;
+        for(int i=0;i<sz(s);i++){
+            mp1[s[i]]++;
+        }
+        for(int i=0;i<sz(target);i++){
+            mp2[target[i]]++;
+        }
+        for(auto it:mp2){
+            ans=min(ans,mp1[it.ff]/it.ss);
+        }
+        if(ans==INT_MAX){
+            ans=0;
+        }
+        return ans;
+    }
+};
+
+
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
---
+++ LeetCode/2295.Replace_Elements_in_an_Array.cpp
@@ -0,0 +1,84 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+*/
+typedef long long ll ;
+typedef vector<int> vl;
+typedef vector<vector<int>> vvl;
+#define speed cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);
+/*  Abbrevations  */
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+// Some print
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+// sort
+#define all(V) (V).begin(),(V).end()
+#define srt(V) sort(all(V))
+#define srtGreat(V) sort(all(V),greater<ll>())
+// some extra
+#define printv(v) for(ll i=0;i<ll(v.size());i++){cout<<v[i]<<" ";} cout<<endl;
+#define precision(x) cout<<fixed<<setprecision(x);
+#define sz(V) ll(V.size())
+
+
+/* ascii value
+A=65,Z=90,a=97,z=122
+*/
+
+/* Some syntax 
+//Syntax to create a min heap for priority queue
+//priority_queue <int, vector<int>, greater<int>>pq;
+*/
+/*  --------------------MAIN PROGRAM----------------------------*/
+// to run ctrl+b
+const ll INF=1e18;
+const ll mod1=1e9+7;
+const ll mod2=998244353;
+// Techniques :
+// divide into cases, brute force, pattern finding
+// sort, greedy, binary search, two pointer
+// transform into graph
+
+// Experience :
+// Cp is nothing but only observation and mathematics.
+
+
+//Add main code here
+
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+        map<int,int> m1,m2;
+        for(int i=0;i<nums.size();i++){
+            m1[nums[i]]=i+1;
+            m2[i+1]=nums[i];
+            // 1 based indexing
+        }
+        for(auto x:operations){
+            int value = x[0],new_vlaue=x[1];
+            int index = m1[value];
+            m2[index]=new_vlaue;
+            m1.erase(value);
+            m1[new_vlaue]=index;
+        }
+        vector<int> ans;
+        for(auto x:m2){
+            ans.pb(x.ss);
+        }
+        return ans;
+
+    }
+};
+
+
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
