87b99253bfa749ed84e39a03a561c8083f274328
Pankajcoder1 <pankajkumarmath1@gmail.com>
2022-06-30 10:53:41 +0530

    some more contest solution

---
+++ Google/KickStart/2016_Round_B-Sherlock_and_Parentheses.cpp
@@ -0,0 +1,111 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll ;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+#define speed cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);
+/*  Abbrevations  */
+#define ff first
+#define ss second
+#define mp make_pair
+#define line cout<<endl;
+#define pb push_back
+// Some print
+#define no cout<<"NO"<<endl;
+#define yes cout<<"YES"<<endl;
+// sort
+#define all(V) (V).begin(),(V).end()
+#define srt(V) sort(all(V))
+#define srtGreat(V) sort(all(V),greater<ll>())
+// some extra
+#define printv(v) for(ll i=0;i<ll(v.size());i++){cout<<v[i]<<" ";} line;
+#define precision(x) cout<<fixed<<setprecision(x);
+#define sz(V) ll(V.size())
+// function
+ll power(ll x,ll y,ll mod)
+{
+    ll res=1;
+    // x=x%mod;
+    while(y>0)
+    {
+        if(y%2==1)
+        {
+            res*=x;
+            // res=res%mod;
+        }
+        y/=2; x*=x; // x=x%mod;
+    }
+    return res;
+}
+ll str_to_num(string s)
+{
+    stringstream pk(s);
+    ll num;
+    pk>>num;
+    return num;
+}
+string num_to_str(ll num)
+{
+    return to_string(num);
+}
+// datatype definination
+#define ordered_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+/* ascii value
+A=65,Z=90,a=97,z=122
+*/
+
+/* Some syntax 
+//Syntax to create a min heap for priority queue
+//priority_queue <int, vector<int>, greater<int>>pq;
+*/
+/*  --------------------MAIN PROGRAM----------------------------*/
+// to run ctrl+b
+const ll INF=1e18;
+const ll mod1=1e9+7;
+const ll mod2=998244353;
+// Techniques :
+// divide into cases, brute force, pattern finding
+// sort, greedy, binary search, two pointer
+// transform into graph
+
+
+// Experience :
+// Cp is nothing but only observation and mathematics.
+
+
+ll solve(){
+    ll l,r;
+    cin>>l>>r;
+    ll mini=min(l,r);
+    ll ans=(mini*(mini+1))/2;
+    cout<<ans<<endl;
+    return 0;
+}
+int main(){
+    speed;
+    /* #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif */
+    ll TestCase=1;
+    cin>>TestCase;
+    for(ll i=1;i<=TestCase;i++){
+        cout<<"Case #"<<i<<": ";
+        solve();
+    }
+}
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
