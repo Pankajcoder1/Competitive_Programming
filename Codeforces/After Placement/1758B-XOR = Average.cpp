95e607ef506ee74734579eb76b9da87cd15e33e7
Pankaj Kumar <pankajkumarmath1@gmail.com>
2022-11-30 19:42:52 +0530

    leetcode

---
+++ Codeforces/After Placement/1758B-XOR = Average.cpp
@@ -0,0 +1,54 @@
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+/* ascii value
+A=65,Z=90,a=97,z=122
+*/
+
+// Techniques :
+// divide into cases, brute force, pattern finding
+// sort, greedy, binary search, two pointer
+// transform into graph
+
+
+int solve(){
+    int n;
+    cin>>n;
+    if(n&1){
+        while(n--){
+            cout<<2<<" ";
+        }
+        cout<<endl;
+    }
+    else{
+        while(n>2){
+            cout<<2<<" ";
+            n--;
+        }
+        cout<<"1 3"<<endl;
+    }
+    return 0;
+}
+int main()
+{
+    long long testCase;
+    cin>>testCase;
+    while(testCase--){
+        solve();
+    }
+    return 0;
+}
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
--- LeetCode/202.Happy_Number.cpp
+++ LeetCode/202.Happy_Number.cpp
@@ -1,25 +1,62 @@
-class Solution {
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+*/
+typedef long long ll ;
+
+
+/* ascii value
+A=65,Z=90,a=97,z=122
+*/
+
+/*  --------------------MAIN PROGRAM----------------------------*/
+// to run ctrl+b
+const ll INF=1e18;
+const ll mod1=1e9+7;
+const ll mod2=998244353;
+// Techniques :
+// divide into cases, brute force, pattern finding
+// sort, greedy, binary search, two pointer
+// transform into graph
+
+// Experience :
+// Cp is nothing but only observation and mathematics.
+
+
+//Add main code here
+
+class Solution
+{
 public:
-    bool isHappy(int n) {
-        map<int,int> m;
-        while(1)
+    bool isHappy(int n)
+    {
+        map<int, int> m;
+        while (1)
         {
-            if(n==1)
+            if (n == 1)
                 return true;
-            else if(m[n])
+            else if (m[n])
                 return false;
             else
             {
                 m[n]++;
-                int temp=0;
-                while(n)
+                int temp = 0;
+                while (n)
                 {
-                    temp+=(n%10)*(n%10);
-                    n/=10;
+                    temp += (n % 10) * (n % 10);
+                    n /= 10;
                 }
-                n=temp;
-                // cout<<"n "<<n<<endl;
+                n = temp;
             }
         }
     }
 };
+
+/* -----------------END OF PROGRAM --------------------*/
+/*
+* stuff you should look before submission
+* constraint and time limit
+* int overflow
+* special test case (n=0||n=1||n=2)
+* don't get stuck on one approach if you get wrong answer
+*/
