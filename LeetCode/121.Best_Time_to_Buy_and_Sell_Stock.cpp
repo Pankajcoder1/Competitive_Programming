ab7459df67c105a85fc935b09abf3a933244329c
Pankaj Kumar <pankajkumarmath1@gmail.com>
2023-01-02 12:12:47 +0530

    leetcode 121 updated

--- LeetCode/121.Best_Time_to_Buy_and_Sell_Stock.cpp
+++ LeetCode/121.Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -1,16 +1,28 @@
-class Solution {
+/*
+    written by Pankaj Kumar.
+    country:-INDIA
+*/
+typedef long long ll ;
+const ll INF=1e18;
+const ll mod1=1e9+7;
+const ll mod2=998244353;
+//Add main code here
+
+class Solution
+{
 public:
-    int maxProfit(vector<int>& prices) {
-        if(prices.size()<2)
+    int maxProfit(vector<int> &prices)
+    {
+        if (prices.size() < 2)
             return 0;
-        int mini=prices[0];
-        int n=prices.size();
-        int maxo=0;
-        for(int i=0;i<n;i++)
+        int mini = prices[0];
+        int n = prices.size();
+        int maxo = 0;
+        for (int i = 1; i < n; i++)
         {
-            mini=min(mini,prices[i]);
-            int diff=prices[i]-mini;
-            maxo=max(diff,maxo);
+            int diff = prices[i] - mini;
+            maxo = max(diff, maxo);
+            mini = min(mini, prices[i]);
         }
         return maxo;
     }
