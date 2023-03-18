9a6cbbebf546fe789e31efbb7c986d79554e6cce
Manish Arya <88669366+Manish989927@users.noreply.github.com>
2023-03-18 23:17:18 +0530

    Update 2592.Maximize Greatness of an Array.cpp

--- LeetCode/2592.Maximize Greatness of an Array.cpp
+++ LeetCode/2592.Maximize Greatness of an Array.cpp
@@ -1,12 +1,7 @@
-/*
-    written by Pankaj Kumar.
-    country:-INDIA
-*/
 typedef long long ll ;
 const ll INF=1e18;
 const ll mod1=1e9+7;
 const ll mod2=998244353;
-//Add main code here
 
 class Solution
 {
@@ -14,19 +9,21 @@ public:
     int maximizeGreatness(vector<int> &nums)
     {
         int n=nums.size();
-        vector<pair<int,int>> v1(n);
+        vector<int> v1(n);
         vector<int> v2(n);
         int count=0;
+        
         for(int i=0;i<n;i++){
-            v1[i]={nums[i],i};
-            v2[i]=nums[i];
+            v1[i]=nums[i];
         }
-        sort(v2.begin(),v2.end());
+        
         sort(v1.begin(),v1.end());
+        v2 = v1;
+         
         int pos=0;
 
         for(int i=0;i<n;i++){
-            while(pos<n && v1[i].>=firstv2[pos]){
+            while(pos<n && v1[i] >=v2[pos]){
                 pos++;
             }
             if(pos<n){
