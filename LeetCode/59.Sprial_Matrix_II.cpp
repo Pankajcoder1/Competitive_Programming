b9086c7fa862202e80b4d30292ea959bbc31dd2a
Pankaj Kumar <pankajkumarmath1@gmail.com>
2023-05-10 12:39:47 +0530

    leetcode 59 modified

--- LeetCode/59.Sprial_Matrix_II.cpp
+++ LeetCode/59.Sprial_Matrix_II.cpp
@@ -2,100 +2,58 @@
     written by Pankaj Kumar.
     country:-INDIA
 */
-typedef long long ll ;
-typedef vector<int> vl;
-typedef vector<vector<int>> vvl;
-#define speed cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);
-/*  Abbrevations  */
-#define ff first
-#define ss second
-#define mp make_pair
-#define pb push_back
-// Some print
-#define no cout<<"NO"<<endl;
-#define yes cout<<"YES"<<endl;
-// sort
-#define all(V) (V).begin(),(V).end()
-#define srt(V) sort(all(V))
-#define srtGreat(V) sort(all(V),greater<ll>())
-// some extra
-#define printv(v) for(ll i=0;i<ll(v.size());i++){cout<<v[i]<<" ";} cout<<endl;
-#define precision(x) cout<<fixed<<setprecision(x);
-#define sz(V) ll(V.size())
+typedef long long ll;
+const ll INF = 1e18;
+const ll mod1 = 1e9 + 7;
+const ll mod2 = 998244353;
+// Add main code here
 
-
-/* ascii value
-A=65,Z=90,a=97,z=122
-*/
-
-/* Some syntax 
-//Syntax to create a min heap for priority queue
-//priority_queue <int, vector<int>, greater<int>>pq;
-*/
-/*  --------------------MAIN PROGRAM----------------------------*/
-// to run ctrl+b
-const ll INF=1e18;
-const ll mod1=1e9+7;
-const ll mod2=998244353;
-// Techniques :
-// divide into cases, brute force, pattern finding
-// sort, greedy, binary search, two pointer
-// transform into graph
-
-// Experience :
-// Cp is nothing but only observation and mathematics.
-
-
-//Add main code here
-
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> ans(n,vector<int>(n,-1));
-        int value=1,i=0,j=0;
-        bool ok=true;
-        while(value<=(n*n)){
-            if(ok){
-                while(j<n && ans[i][j]==-1){
-                    ans[i][j]=value++;
-                    j++;
-                }
-                j--;
-                i++;
-                while(i<n && ans[i][j]==-1){
-                    ans[i][j]=value++;
-                    i++;
-                }
-                i--;
-                j--;
-                ok=false;
+    vector<vector<int>> generateMatrix(int n)
+    {
+        vector<vector<int>> res(n, vector<int>(n));
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+        int val = 1;
+
+        while (top <= bottom && left <= right)
+        {
+            // Traverse top row
+            for (int i = left; i <= right; i++)
+            {
+                res[top][i] = val++;
             }
-            else{
-                while(j>=0 && ans[i][j]==-1){
-                    ans[i][j]=value++;
-                    j--;
+            top++;
+
+            // Traverse right column
+            for (int i = top; i <= bottom; i++)
+            {
+                res[i][right] = val++;
+            }
+            right--;
+
+            // Traverse bottom row
+            if (top <= bottom)
+            {
+                for (int i = right; i >= left; i--)
+                {
+                    res[bottom][i] = val++;
                 }
-                j++;
-                i--;
-                while(i>=0 && ans[i][j]==-1){
-                    ans[i][j]=value++;
-                    i--;
+                bottom--;
+            }
+
+            // Traverse left column
+            if (left <= right)
+            {
+                for (int i = bottom; i >= top; i--)
+                {
+                    res[i][left] = val++;
                 }
-                i++;
-                j++;
-                ok=true;
+                left++;
             }
         }
-        return ans;
+
+        return res;
     }
 };
-
-
-/* -----------------END OF PROGRAM --------------------*/
-/*
-* stuff you should look before submission
-* constraint and time limit
-* int overflow
-* special test case (n=0||n=1||n=2)
-* don't get stuck on one approach if you get wrong answer
-*/
